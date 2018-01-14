14f11e67a7243f1c4829708446fdf86591009faf
Matthias Hertel <me@matthiashertel.eu>
2018-01-14 17:40:30 +0100

    credentials come from environment

--- web_django/config/settings/base.py
+++ web_django/config/settings/base.py
@@ -274,10 +274,11 @@ ADMIN_URL = r'^admin/'
 # Your common stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
 
+
 # MOVES specific configuration
 MOVES = {
-    'client_id': 'nXLqaAKvk5nnptyNEFr2A686JslcdGm8',
-    'client_secret': 'K2MIRzvO2T6Uy8o7o6qG4NwHTvZiD2QWorT6_7iYU41BVk4HzwDG7jn6h6fGX0z3',
-    'api_auth': 'https://api.moves-app.com/oauth/v1',
-    'api': 'https://api.moves-app.com/api/1.1'
+    'client_id': env('CLIENT_ID') ,
+    'client_secret': env('CLIENT_SECRET') ,
+    'api_auth': env('API_AUTH'),
+    'api': env('API')
 }
