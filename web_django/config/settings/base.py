a36b704bc9e2c21439b4c0d5bb910bdb9dfcfad8
Matthias Hertel <me@matthiashertel.eu>
2018-02-07 20:13:33 +0100

    guter stand ... ein guter ansatz queueing zu haben fuer jobs

--- web_django/config/settings/base.py
+++ web_django/config/settings/base.py
@@ -268,6 +268,23 @@ LOGIN_URL = 'account_login'
 # SLUGLIFIER
 AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 
+########## CELERY
+INSTALLED_APPS += ['django_playground.taskapp.celery.CeleryConfig']
+# CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='django://')
+# if CELERY_BROKER_URL == 'django://':
+#     CELERY_RESULT_BACKEND = 'redis://'
+# else:
+#     CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+
+BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Berlin'
+########## END CELERY
+
+
 
 # Location of root django.contrib.admin URL, use {% url 'admin:index' %}
 ADMIN_URL = r'^admin/'
