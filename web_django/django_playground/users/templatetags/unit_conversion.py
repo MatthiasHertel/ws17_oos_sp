c4720f933bdf9155c00869facb1524353f121243
Arne Wieding <arne@wieding.com>
2018-02-09 11:40:41 +0100

    added conversion of calories to kilojoules and Watts

--- web_django/django_playground/users/templatetags/unit_conversion.py
+++ web_django/django_playground/users/templatetags/unit_conversion.py
@@ -13,7 +13,7 @@ def format_meters(value, unit):
     if unit == 'mi':
         return '{:.0f} miles'.format(Distance(m=value).mi)
 
-    return Distance(m=value).m
+    return '{:.0f} meters'.format(Distance(m=value).m)
 
 
 @register.filter(name='format_miles')
@@ -36,3 +36,13 @@ def format_seconds(value):
 def format_pizza(calories):
     pizza_calories = 900
     return '{} which equals {:.0f} pizzas!'.format(calories, int(calories)/pizza_calories)
+
+
+@register.filter(name='calories_to_kj')
+def calories_to_kj(calories):
+    return '{:.0f} kJ'.format(int(calories)*4.184)
+
+
+@register.filter(name='calories_to_watts')
+def calories_to_watts(calories, duration):
+    return '{:.0f} Watts'.format(int(calories)*4184/duration)
