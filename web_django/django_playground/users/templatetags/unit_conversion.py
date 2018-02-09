4d711bde532bdb6585637217e7467cf9db306fda
Arne Wieding <arne@wieding.com>
2018-02-09 07:49:43 +0100

    template formatting and filters

---
+++ web_django/django_playground/users/templatetags/unit_conversion.py
@@ -0,0 +1,21 @@
+from django import template
+from django.contrib.gis.measure import Distance
+import datetime
+
+register = template.Library()
+
+
+@register.filter(name='format_meters')
+def format_meters(value, unit):
+    if unit == 'km':
+        return '{:.0f} km'.format(Distance(m=value).km)
+
+    if unit == 'mi':
+        return '{:.0f} miles'.format(Distance(m=value).mi)
+
+    return Distance(m=value).m
+
+
+@register.filter(name='format_seconds')
+def format_meters(value):
+    return '{}'.format(datetime.timedelta(seconds=value))
